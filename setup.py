d342f019c57efeca6fc6801af78c367f72775d6e
Jozef Knaperek <jozef.knaperek@01cryptohouse.com>
2021-01-07 10:50:06 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except LookupError:
     ascii = codecs.lookup('ascii')
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == 'mbcs'))
 
-VERSION = '1.0.6'
+VERSION = '1.0.7'
 
 setup(
     name='onegramlib',
